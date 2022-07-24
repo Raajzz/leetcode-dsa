2d0e60873e4e6e01dbf2483027e9697a64e09c33
Raaj_S <72331459+Raajzz@users.noreply.github.com>
2022-07-24 13:16:12 +0530

    Time: 201 ms, Memory: 51.4 MB - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,bool> umap;
-        int counter = 0;
+        // unordered_map<int,bool> umap;
+        // int counter = 0;
+        // for(auto val : nums){
+        //     if(umap[val] != 0){
+        //         return true;
+        //     } else {
+        //         umap[val] = true;
+        //     }
+        // }
+        // return false;
+        unordered_set<int> uset;
         for(auto val : nums){
-            if(umap[val] != 0){
+            if(uset.find(val) != uset.end()){
                 return true;
             } else {
-                umap[val] = true;
+                uset.insert(val);
             }
         }
         return false;
