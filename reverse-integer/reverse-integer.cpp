6f03a78e0d9dda162dbfd0f98eddc47f7653e792
Raaj_S <72331459+Raajzz@users.noreply.github.com>
2022-07-27 22:11:54 +0530

    Time: 0 ms, Memory: 6 MB - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string str_x = to_string(x);
+        int first = (str_x[0]=='-')?1:0;
+        std::reverse(str_x.begin()+first, str_x.end());
+        string str_max = to_string(INT_MAX);
+        string str_min = to_string(INT_MIN);
+        if(first == 0){
+            if(str_x.size() < str_max.size()){
+                return atoi(str_x.c_str());
+            } else if(str_x.size() > str_max.size()){
+                return 0;
+            } else {
+                for(int i=0; i<str_x.size(); i++){
+                    if(str_x[i] > str_max[i]){
+                        return 0;
+                    } else if(str_x[i] < str_max[i]){
+                        return atoi(str_x.c_str());
+                    }
+                }
+                return atoi(str_x.c_str());
+            }
+        } else if (first == 1){
+            if(str_x.size() < str_min.size()){
+                return atoi(str_x.c_str());
+            } else if(str_x.size() > str_min.size()){
+                return 0;
+            } else {
+                for(int i=0; i<str_x.size(); i++){
+                    if(str_x[i] > str_min[i]){
+                        return 0;
+                    } else if(str_x[i] < str_min[i]){
+                        return atoi(str_x.c_str());
+                    }
+                }
+                return atoi(str_x.c_str());
+            }
+        }
+        return 0;
+    }
+};
