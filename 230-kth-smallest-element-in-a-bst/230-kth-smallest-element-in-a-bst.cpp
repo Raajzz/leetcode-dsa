05eb4a212428c9081e1f5f37ea6c22f55eefcd42
Raaj_S <72331459+Raajzz@users.noreply.github.com>
2022-09-15 10:23:18 +0530

    Time: 24 ms (72.42%), Space: 24.2 MB (34.03%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,19 +11,45 @@
  */
 class Solution {
 public:
-    // brute forced O(N) time and O(N) space approach
-    void getElements(TreeNode* root, vector<int> &elements){
-        if(root != nullptr){
-            getElements(root->left, elements);
-            elements.push_back(root->val);
-            getElements(root->right, elements);
+    
+//     brute forced O(N) time and O(N) space approach - works
+//     void getElements(TreeNode* root, vector<int> &elements){
+//         if(root != nullptr){
+//             getElements(root->left, elements);
+//             elements.push_back(root->val);
+//             getElements(root->right, elements);
+//         }
+//     }
+    
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> elements;
+//         getElements(root, elements);
+//         return elements[k-1];
+//     }
+    
+//     optimal method - O(N) time, usage of an external counter while doing inorder 
+//     traversal
+    
+    void getNumber(TreeNode* root, int pos, int &counter, int &res, bool &isFound){
+        if(root != nullptr && !isFound){
+            getNumber(root->left, pos, counter, res, isFound);
+            if(counter == pos && !isFound){
+                isFound = true;
+                res = root->val;
+                return;
+            } else {
+                counter++;
+            }
+            getNumber(root->right, pos, counter, res, isFound);
         }
     }
     
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> elements;
-        getElements(root, elements);
-        return elements[k-1];
+    int kthSmallest(TreeNode* root, int k){
+        int res = 0;
+        int counter = 1;
+        bool isFound = false;
+        getNumber(root, k, counter, res, isFound);
+        return res;
     }
     
 };
