e0c34de48799d088587767064de0c7be7b20363a
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-06 15:35:48 +0530

    Time: 55 ms (22.41%), Space: 82.1 MB (5.86%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int recursion(string word1, string word2, int index1, int index2, vector<vector<int>> &dp){
+        if(index1 < 0){
+            return index2 + 1;
+        }
+        if(index2 < 0){
+            return index1 + 1;
+        }
+        if(dp[index1][index2] != -1){
+            return dp[index1][index2];
+        }
+        if(word1[index1] == word2[index2]){
+            return dp[index1][index2] = recursion(word1, word2, index1 - 1, index2 - 1, dp);
+        }
+        return dp[index1][index2] = min(
+            1 + recursion(word1, word2, index1 - 1, index2, dp),
+            min(
+                1 + recursion(word1, word2, index1 - 1, index2 - 1, dp),
+                1 + recursion(word1, word2, index1, index2 - 1, dp)
+            )
+        );
+    }
+    
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
+        return recursion(word1, word2, word1.size() - 1, word2.size() - 1, dp);
+    }
+};
