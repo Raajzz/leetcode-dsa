c31aaa1d1203486e9ad55edf82b18d7ace40496f
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-07 00:46:17 +0530

    Time: 1540 ms (16.51%), Space: 422.5 MB (9.39%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int recursion(int i, int j, string s, string t, vector<vector<int>> &dp){
+        if(j < 0){
+            return 1;
+        }
+        if(i < 0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s[i] == t[j]){
+            return dp[i][j] = recursion(i - 1, j - 1, s, t, dp) + recursion(i - 1, j, s, t, dp);
+        }
+        return dp[i][j] = recursion(i - 1, j, s, t, dp);
+    }
+    
+    int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
+        return recursion(s.size() - 1, t.size() - 1, s, t, dp);
+    }
+};
