b7744f46d8cebce432fe250fd0cd54c6c4620e51
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-13 00:14:21 +0530

    Time: 10 ms (44.71%), Space: 15.2 MB (25.10%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,8 +22,32 @@ public:
         return dp[index][buy] = profit;
     }
     
+    int tabulation(vector<int> &prices){
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(2, 0));
+        dp[prices.size()][0] = dp[prices.size()][1] = 0;
+        
+        for(int index=prices.size() - 1; index>=0; index--){
+            for(int buy = 0; buy <= 1; buy++){
+                int profit = 0;
+                if(buy == 0){
+                    profit = max(
+                        -prices[index] + dp[index + 1][1], 
+                        dp[index + 1][0]
+                    );
+                } else {
+                    profit = max(
+                        prices[index] + dp[index + 1][0],
+                        dp[index + 1][1]
+                    );
+                }
+                dp[index][buy] = profit;
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
-        return recursion(prices, 0, 0, dp);
+        return tabulation(prices);
     }
 };
