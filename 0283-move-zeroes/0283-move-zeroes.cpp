91d33a64d6462e3ca2b13d56242b3e11b6d12774
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-13 14:00:26 +0530

    Time: 18 ms (83.52%), Space: 19.3 MB (31.20%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeroPtr = 0;
-        int numPtr = 0;
-        while(zeroPtr < nums.size() && numPtr < nums.size()){
-            while(zeroPtr < nums.size() && nums[zeroPtr] != 0){
-                cout << zeroPtr << " ";
-                zeroPtr++;
+        int snowBall = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0){
+                snowBall++;
+            } else if (snowBall > 0){
+                nums[i - snowBall] = nums[i];
+                nums[i] = 0;
             }
-            cout << "\n";
-            numPtr = zeroPtr;
-            while(numPtr < nums.size() && nums[numPtr] == 0){
-                numPtr++;
-            }
-            cout << zeroPtr << " " << numPtr << "\n";
-            if(numPtr >= nums.size() || zeroPtr >= nums.size()){
-                break;
-            }
-            swap(nums[numPtr], nums[zeroPtr]);
         }
     }
 };
