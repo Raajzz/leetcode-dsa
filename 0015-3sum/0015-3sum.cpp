18888719e8d16125a5b1a717d4e7c44e0d1cbf26
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-14 20:55:09 +0530

    Time: 163 ms (66.95%), Space: 24.1 MB (71.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // sort the array
         sort(nums.begin(), nums.end());
-        set<vector<int>> st;
-        for(int i=0; i<nums.size(); i++){
-            int j = 0;
+        int i = 0;
+        vector<vector<int>> res;
+        while(i < nums.size()){
+            if(i > 0 && nums[i] == nums[i-1]){
+                i++;
+                continue;
+            }
+            int j = i + 1;
             int k = nums.size() - 1;
             while(j < k){
-                if(j == i){
-                    j++;
-                    continue;
-                }
-                if(k == i){
-                    k--;
-                    continue;
-                }
                 int currRes = nums[i] + nums[j] + nums[k];
                 if(currRes > 0){
                     k--;
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
                 } else if (currRes < 0){
                     j++;
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
                 } else {
-                    vector<int> tempSt = {nums[i], nums[j], nums[k]};
-                    sort(tempSt.begin(), tempSt.end());
-                    st.insert(tempSt);
+                    res.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
                 }
             }
+            i++;
         }
-        vector<vector<int>> res(st.begin(), st.end());
         return res;
     }
 };
