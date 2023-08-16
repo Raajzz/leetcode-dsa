5a4cce36c75adcdb03ac14b465c5b857282f9b40
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-16 19:40:52 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (64.14%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -12,17 +12,18 @@ public:
             if(arr[mid] == arr[low] && arr[mid] == arr[high]){
                 low++;
                 high--;
-            } else if (arr[mid] <= arr[high]){
-                if(arr[mid] < target && target <= arr[high]){
-                    low = mid + 1;
-                } else {
+            } else if(arr[low] <= arr[mid]) {
+                if(arr[low] <= target && target <= arr[mid]){
                     high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
-            } else {
-                if(arr[low] <= target && target < arr[mid]){
-                    high = mid - 1;
-                } else {
+            }
+            else {
+                if(arr[mid] <= target && target <= arr[high]){
                     low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
         }
