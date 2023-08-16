cb11cdb1e6285d363d31a540942e69a5fb4c18a4
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-16 10:36:36 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (32.34%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        // int flag = 0;
-        // for(int i=0; (i<nums.size() - 1) && (flag != 2); i++){
-        //     if(flag == 0 || flag == 1){
-        //         if( nums[i] > nums[i+1] ){
-        //             flag++;
-        //         }
-        //     }
-        // }
-        // return (flag == 0 || flag == 1);
-        
-        // infliction point finder
-        
-        int inflictionPoint = -1;
-        int flag = 0;
+        int inflictionPoint = 0;
         for(int i=0; i<nums.size() - 1; i++){
             if(nums[i] > nums[i+1]){
-                flag += 1;
-                inflictionPoint = i;
+                inflictionPoint++;
             }
         }
-        
-        if(flag > 1){
-            return false;
-        }
-        
-        if(inflictionPoint == -1){
+        if(inflictionPoint == 0){
             return true;
-        } else {
-            if(nums[nums.size() - 1] > nums[0]){
-                return false;
-            }
-        }
-        return true;
+        } else if(inflictionPoint == 1){
+            return nums[nums.size() - 1] <= nums[0];
+        } 
+        return false;
     }
 };
