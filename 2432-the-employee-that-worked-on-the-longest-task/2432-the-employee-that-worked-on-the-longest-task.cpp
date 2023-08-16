c254d9b82b0a2a38f07e421f96f6102993d7db1b
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-16 11:46:52 +0530

    Time: 70 ms (74.36%), Space: 39 MB (6.62%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        unordered_map<int,int> mp;
+        int currTime = 0;
+        int maxTaskTime = INT_MIN;
+        for(auto vec: logs){
+            int taskTime = vec[1] - currTime;
+            maxTaskTime = max(maxTaskTime, taskTime);
+            if(mp.find(taskTime) == mp.end()){
+                mp.insert(make_pair(taskTime, vec[0]));
+            } else {
+                if(mp[taskTime] > vec[0]){
+                    cout << taskTime << " " << vec[0] << "\n";
+                    mp[taskTime] = vec[0];
+                }
+            }
+            currTime = vec[1];
+        }
+        return mp[maxTaskTime];
+    }
+};
