61047e565117bbd8b196f7e059bddce134a575cc
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-18 00:29:07 +0530

    Time: 110 ms (69.65%), Space: 93.4 MB (34.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,7 @@ public:
         int profit = 0;
         for(int i=prices.size()-2; i>=0; i--){
             maxPrice = max(prices[i], maxPrice);
-            if(prices[i] < maxPrice){
-                profit = max(maxPrice - prices[i], profit);
-            }
+            profit = max(maxPrice - prices[i], profit);
         }
         return profit;
     }
