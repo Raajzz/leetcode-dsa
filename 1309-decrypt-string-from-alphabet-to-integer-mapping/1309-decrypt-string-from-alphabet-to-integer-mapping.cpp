2005e90da9d246f627d5594afd959b8a5fde3ea8
Raaj S <72331459+Raajzz@users.noreply.github.com>
2023-08-21 22:43:41 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (20.08%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     string freqAlphabets(string s) {
+        /********************************************************************
+        
+        - a different way or better way of doing this would be to start from the beginning of the string. Check of i + 2 is a '#' if that's the case then take the first character, convert to a number, multiply it by 10 and then convert it back to the respective character. If the i + 2th string isn't an '#' just convert the current character to its respective alphabet and be done with it.
+        
+        - Refer to the below solution
+        
+        string freqAlphabets(string s) {
+            int n = s.length(), i = 0;
+            string ans = "";
+            while(i < n)
+                if(i + 2 < n && s[i + 2] == '#') {
+                    int ss = (s[i]-'0')*10 + (s[i+1]-'0');
+                    ans += char(ss+'a'-1);
+                    i += 3;
+                }
+                else {
+                    ans += char(s[i]-'0'-1+'a');
+                    i++;
+                }
+            }
+            return ans;
+        }
+        
+        ********************************************************************/
         int i = s.size() - 1;
         string res = "";
         while(i >= 0){
